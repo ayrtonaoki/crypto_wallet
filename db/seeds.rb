6c2f0f36dc419736604cff8b42853ff7dbe776d0
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-06-21 19:58:48 -0300

    Create seeds for coins

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Coin.create!(
+  description: 'Bitcoin',
+  acronym: 'BTC',
+  url_image: 'https://w7.pngwing.com/pngs/450/133/png-transparent-bitcoin-cryptocurrency-virtual-currency-decal-blockchain-info-bitcoin-text-trademark-logo.png'
+)
+
+Coin.create!(
+  description: 'Ethereum',
+  acronym: 'ETH',
+  url_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/471px-Ethereum_logo_2014.svg.png'
+)
+
+Coin.create!(
+  description: 'Dash',
+  acronym: 'DASH',
+  url_image: 'https://cdn.freebiesupply.com/logos/large/2x/dash-3-logo-png-transparent.png'
+)
