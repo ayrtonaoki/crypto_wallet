17dae14d0471d59bf50dc1a4d8851e4e15177a2e
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-06-23 17:04:09 -0300

    Rollback mining type migrate and recreate scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_165944) do
+ActiveRecord::Schema.define(version: 2020_06_23_182506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2020_06_23_165944) do
   end
 
   create_table "mining_types", force: :cascade do |t|
-    t.string "name"
+    t.string "description"
     t.string "acronym"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
